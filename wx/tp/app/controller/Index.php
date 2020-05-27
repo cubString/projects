29c4408278b4d0460f6dc475bcd370cd81fe8d72
j <sjiaina@sina.com>
2020-05-27 18:41:55 +0800

    Update Index.php

--- wx/tp/app/controller/Index.php
+++ wx/tp/app/controller/Index.php
@@ -10,11 +10,11 @@ class Index extends BaseController
     public function index() 
     {
 		$data = Db::table('s_user')->select();
-		
+
 		 // 模板变量赋值
 		View::assign('name','ThinkPHP');
 		// 模板输出
-        return View::fetch('index');
+        return View::fetch();
     }
 
     public function hello($name = 'ThinkPHP6')
