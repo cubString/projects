3d09e47a1b28b14d2aae9145fe4de3ee4641fe31
j <sjiaina@sina.com>
2020-05-27 16:48:37 +0800

    Update index.php

--- wx/tp/public/index.php
+++ wx/tp/public/index.php
@@ -11,19 +11,15 @@
 
 // [ 应用入口文件 ]
 namespace think;
-var_dump(__DIR__);
-var_dump(__DIR__.'/../vendor/autoload.php');
-var_dump(is_file(__DIR__.'/../vendor/autoload.php'));
 
-exit;
 require __DIR__ . '/../vendor/autoload.php';
 
-echo "isOK1";exit;
+
 // 执行HTTP应用并响应
 $http = (new App())->http;
-echo "isOK2";exit;
+
 $response = $http->run();
-echo "isOK3";
+
 $response->send();
-echo "isOK4";exit;
+
 $http->end($response);
